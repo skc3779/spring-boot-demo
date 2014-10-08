6bec5e34e0a0fa0439e974c3b65055e2429d4b6a
seo kang chun <skc3779@gmail.com>
2014-10-08 10:22:04 +0900

    구조 일부수정

--- src/main/java/boot/app/Application.java
+++ src/main/java/boot/app/Application.java
@@ -2,8 +2,10 @@ package boot.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 /**
  * Created by kaha on 2014. 10. 7..
@@ -16,7 +18,14 @@ import org.springframework.context.annotation.Configuration;
         "boot.service",
         "boot.controller"})
 public class Application {
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        return filter;
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
     }
 }
